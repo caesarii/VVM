8066de7bde050c553434eceb2bf59e61d836b022
caesarii <iwangqinghe@gmail.com>
2018-03-22 10:53:25 +0800

    create registers

---
+++ micro/Register.js
@@ -0,0 +1,50 @@
+const Base = require('./BaseObject')
+
+const Bus = require('./bus')
+
+class Register extends Base {
+    constructor() {
+        super()
+        this.value = null
+        this.enableRead = true
+        this.enableWrite = true
+    }
+    
+    read() {
+        if(this.enableRead) {
+            Bus.B = this.value
+        }
+    }
+    
+    write() {
+        if(this.enableWrite) {
+            this.value = Bus.C
+        }
+    }
+}
+
+class Registers {
+    constructor() {
+        // 寄存器 10
+        // 内存地址寄存器
+        this.MAR = Register.new()
+        // 内存数据寄存器
+        this.MDR = Register.new()
+        //
+        this.PC = Register.new()
+        
+        this.MBR = Register.new()
+        
+        this.SP = Register.new()
+        
+        this.LV = Register.new()
+        
+        this.CPP = Register.new()
+        
+        this.TOS = Register.new()
+        
+        this.OPC = Register.new()
+        
+        this.H = Register.new()
+    }
+}
