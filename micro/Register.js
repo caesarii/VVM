974063ee064ad6d504047dbf3aa5885214fc083f
caesarii <iwangqinghe@gmail.com>
2018-03-23 14:30:42 +0800

    update register

--- micro/Register.js
+++ micro/Register.js
@@ -30,24 +30,25 @@ class Registers {
         this.MAR = Register.new()
         // 内存数据寄存器
         this.MDR = Register.new()
-        //
+        // 指向下一条指令的地址
         this.PC = Register.new()
-        
+        //
         this.MBR = Register.new()
-        
         // 无符号 MBR
         this.MBRU = Register.new()
         
+        // 指向内存中操作数栈的栈顶
         this.SP = Register.new()
-        
+        // 局部变量结构的起始处
         this.LV = Register.new()
-        
+        // 常量池的起始处
         this.CPP = Register.new()
-        
+        //
         this.TOS = Register.new()
         
         this.OPC = Register.new()
         
+        // A 输入
         this.H = Register.new()
     }
 }
